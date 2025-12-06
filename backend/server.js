2a61c8505a91aaa66fd8b0a452ba68834b87c7b7
Amit katiyar <amitkatiyar.jnu08@gmail.com>
2025-12-06 18:15:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '1mb' }));
 
-app.use('/api', (req, res, next) => {
+app.use('/api/admin', (req, res, next) => {
     res.set('X-Robots-Tag', 'noindex');
     next();
 });
